6f5b3177dea14f8df26294abccb926107045d00b
insolor <2442833+insolor@users.noreply.github.com>
2025-01-14 16:34:30 +0300

    Add domain parameter to GettextWrapper and pass languages to translation method explicitely

--- streamlit_gettext/streamlit_gettext.py
+++ streamlit_gettext/streamlit_gettext.py
@@ -3,8 +3,12 @@ from __future__ import annotations
 import gettext as _gettext_module
 import re
 from pathlib import Path
+from typing import TYPE_CHECKING
 
 import streamlit as st
+
+if TYPE_CHECKING:
+    from collections.abc import Iterable
 
 
 def get_preferred_languages() -> list[str]:
@@ -16,24 +20,25 @@ class GettextWrapper:
     """
     A wrapper for the gettext module
     """
-
     locale_path: Path
+    domain: str
 
-    def __init__(self, locale_path: str | Path) -> None:
+    def __init__(self, locale_path: str | Path, domain: str = "messages") -> None:
         self.locale_path = Path(locale_path)
+        self.domain = domain
 
-    def _translation(self) -> _gettext_module.NullTranslations:
+    def translation(self, languages: Iterable[str] | None = None) -> _gettext_module.NullTranslations:
         return _gettext_module.translation(
-            "messages",
+            domain=self.domain,
             localedir=self.locale_path,
-            languages=get_preferred_languages(),
+            languages=languages,
             fallback=True,
         )
 
     def gettext(self, message: str) -> str:
-        translation = self._translation()
+        translation = self.translation(get_preferred_languages())
         return translation.gettext(message)
 
     def ngettext(self, singular: str, plural: str, n: int) -> str:
-        translation = self._translation()
+        translation = self.translation(get_preferred_languages())
         return translation.ngettext(singular, plural, n)
